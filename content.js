22b76e7f2dd53147f1489a2f1cfd4d2804d262c8
Stefan Maier <tliff@tliff.de>
2019-07-17 16:26:44 +0200

    Fix length of middle substitutions array

--- content.js
+++ content.js
@@ -54,12 +54,12 @@ for (var i = 0; i < elements.length; i++) {
         var node = element.childNodes[j];
 
         var replacement = substitutionsFirst[Math.floor(Math.random() * substitutionsFirst.length)] +
-            " " + substitutionsMiddle[Math.floor(Math.random() * substitutionsFirst.length)] +
+            " " + substitutionsMiddle[Math.floor(Math.random() * substitutionsMiddle.length)] +
             "-" + substitutionsLast[Math.floor(Math.random() * substitutionsLast.length)]
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/Annegret Kramp-Karrenbauer|Kramp-Karrenbauer/gi, replacement);
+            var replacedText = text.replace(/Bannegret Krank-Keilerhauer|Bannegret Krank-Keilerhauer/gi, replacement);
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
